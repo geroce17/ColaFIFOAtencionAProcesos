80e42b5b06669e90c495e37427f5bdf4bc82aa63
geroce17 <geroce@outlook.com>
2017-06-14 22:36:32 -0500

    Version 1

---
+++ Procesador.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC___Atencion_a_procesos
+{
+    class Procesador
+    {
+        private int NoProceso = 0;
+        public Proceso inicio { get; set; }
+        public int Pen, suma;
+        private Proceso final;
+        private Proceso actual;
+        private static Random ciclos = new Random();
+
+        public void crearProceso()
+        {
+            Proceso nuevo = new Proceso();
+            nuevo.nombre = Convert.ToString("Proceso " + NoProceso);
+            nuevo.ciclos = ciclos.Next(4, 15);
+            if (inicio == null)
+            {
+                inicio = nuevo;
+                final = nuevo;
+            }
+            else
+            {
+                actual = inicio;
+                while (actual.siguiente != null)
+                {
+                    actual = actual.siguiente;
+                }
+                actual.siguiente = nuevo;
+                nuevo.anterior = actual;
+                final = nuevo;
+            }
+            NoProceso++;
+        }
+        public void eliminarProceso()
+        {
+            inicio.ciclos--;
+            if (inicio == final && inicio.ciclos == 0)
+            {
+                inicio = null;
+                final = null;
+            }
+            else
+            {
+                if (inicio.ciclos == 0)
+                {
+                    inicio.siguiente.anterior = null;
+                    inicio = inicio.siguiente;
+                }
+            }
+        }
+
+        public void pendientesSuma()
+        {
+            if (inicio == null)
+            {
+                Pen = 0;
+                suma = 0;
+            }
+            else
+            {
+                actual = inicio;
+                while (actual != null)
+                {
+                    Pen++;
+                    suma = suma + actual.ciclos;
+                    actual = actual.siguiente;
+                }
+            }
+        }
+    }
+}
